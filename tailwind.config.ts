602c47f5634a4f368e61c7a3a4c670981e19c1a6
Heesho <hisham11027@gmail.com>
2025-10-28 01:29:30 -0700

    fixing build error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss"
 import defaultTheme from "tailwindcss/defaultTheme"
 
 export default {
-  darkMode: ["class"],
+  darkMode: "class",
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
     extend: {
